3a8d41ba821efdf279949cd768e7dbe89a3f7a0a
Marijn-dev <marijnruiter09@gmail.com>
2026-05-06 11:52:13 +0200

    removed unused models

--- src/indago/model.py
+++ src/indago/model.py
@@ -35,119 +35,6 @@ class Dynamics_JAX:
         return self._dx(t, x, u)
 
 
-"""Diffrax implementation of Param CTM model (smooth)"""
-
-
-class ParameterisedCellTransmissionModelSmooth_Diffrax(Dynamics_JAX):
-    def __init__(self, dynamics: ParameterisedCellTransmissionModel, delta):
-        super().__init__(dynamics, delta)
-        self.delta = delta
-        self.inv_step = dynamics.dynamics.inv_step
-        self.locs = None
-        self.vals = None
-        self.sigma_i = None
-        self.flux = self.flux_nointerp  # flux_interp / flux_nointerp
-
-    def flux_nointerp(self, x):
-        return jnp.minimum(
-            self.q_max * x / self.sigma_i,
-            (1 - x) * self.q_max / (1 - self.sigma_i),
-        )
-
-    def flux_interp(self, x):
-        locs = jnp.array([0.0, self.sigma_i, 1.0])
-        vals = jnp.array([0.0, self.q_max, 0.0])
-        return jnp.interp(x, locs, vals)
-
-    def _set_parameter(self, parameter):
-        self.sigma_i = parameter[0]
-        self.q_max = parameter[1]
-
-    def a(self, y1, y2, gamma=10):
-        return jnp.exp(gamma * y1) / (jnp.exp(gamma * y1) + jnp.exp(gamma * y2))
-
-    def softmax(self, y_1, y_2):
-        a_y = self.a(y_1, y_2)
-        return a_y * y_1 + (1 - a_y) * y_2
-
-    def softmin(self, y_1, y_2):
-        a_y = self.a(-y_1, -y_2)
-        return a_y * y_1 + (1 - a_y) * y_2
-
-    def _dx(self, t, x, u):
-        index = jnp.floor(t / self.delta).astype(jnp.uint32)
-        u_val = u[index]
-        x_minus_one = jnp.roll(x, 1).at[0].set(u_val[0])
-        x_plus_one = jnp.roll(x, -1).at[-1].set(0.0)
-
-        D_i_minus_one = self.flux(self.softmin(x_minus_one, self.sigma_i))
-        S_i = self.flux(self.softmax(x, self.sigma_i))
-        q_i_minus_one = self.softmin(D_i_minus_one, S_i)
-
-        D_i = self.flux(self.softmin(x, self.sigma_i))
-        S_i_plus_one = self.flux(self.softmax(x_plus_one, self.sigma_i))
-        q_i = self.softmin(D_i, S_i_plus_one)
-
-        dx = self.inv_step * (q_i_minus_one - q_i)
-        return dx
-
-    def __call__(self, t, x, args):
-        u, params = args
-        self._set_parameter(params)
-        return self._dx(t, x, u)
-
-
-"""Diffrax implementation of Param CTM model (nonsmooth)"""
-
-
-class ParameterisedCellTransmissionModelNonSmooth_Diffrax(Dynamics_JAX):
-    def __init__(self, dynamics: ParameterisedCellTransmissionModel, delta):
-        super().__init__(dynamics, delta)
-        self.delta = delta
-        self.inv_step = dynamics.dynamics.inv_step
-        self.locs = None
-        self.vals = None
-        self.sigma_i = None
-        self.flux = self.flux_interp  # flux_interp / flux_nointerp
-
-    def flux_nointerp(self, x):
-        return jnp.minimum(
-            self.q_max * x / self.sigma_i,
-            (1 - x) * self.q_max / (1 - self.sigma_i),
-        )
-
-    def flux_interp(self, x):
-        locs = jnp.array([0.0, self.sigma_i, 1.0])
-        vals = jnp.array([0.0, self.q_max, 0.0])
-        return jnp.interp(x, locs, vals)
-
-    def _set_parameter(self, parameter):
-        self.sigma_i = parameter[0]
-        self.q_max = parameter[1]
-
-    def _dx(self, t, x, u):
-        index = jnp.floor(t / self.delta).astype(jnp.uint32)
-        u_val = u[index]
-        x_minus_one = jnp.roll(x, 1).at[0].set(u_val[0])
-        x_plus_one = jnp.roll(x, -1).at[-1].set(0.0)
-
-        D_i_minus_one = self.flux(jnp.minimum(x_minus_one, self.sigma_i))
-        S_i = self.flux(jnp.maximum(x, self.sigma_i))
-        q_i_minus_one = jnp.minimum(D_i_minus_one, S_i)
-
-        D_i = self.flux(jnp.minimum(x, self.sigma_i))
-        S_i_plus_one = self.flux(jnp.maximum(x_plus_one, self.sigma_i))
-        q_i = jnp.minimum(D_i, S_i_plus_one)
-
-        dx = self.inv_step * (q_i_minus_one - q_i)
-        return dx
-
-    def __call__(self, t, x, args):
-        u, params = args
-        self._set_parameter(params)
-        return self._dx(t, x, u)
-
-
 """Jax implementation of Param CTM model, used for (manual) euler and diffrax integration"""
 
 
@@ -200,7 +87,7 @@ class ParameterisedCellTransmissionModel_Jax:
         x_last, xs = jax.lax.scan(f, x, us)
         return jnp.concatenate((xs, jnp.expand_dims(x_last, 0)), axis=0)
 
-    # We use this call for in diffrax eq solve
+    # We use this call for diffrax eq solve
     def __call__(self, t, x, args):
         u, params = args
         index = jnp.floor(t / self.delta).astype(jnp.uint32)
@@ -286,9 +173,7 @@ class ParameterisedVanDerPol_Numpy:
         p, v = x
 
         dp = v
-        dv = (
-            -p + self.damping * (1 - p**2) * v + u_val[0]
-        ) 
+        dv = -p + self.damping * (1 - p**2) * v + u_val[0]
 
         return (dp, dv)
 
